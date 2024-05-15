d0a55559cdaafe6485365e59518477cc1075e1be
Aral <me@aral.cc>
2024-05-15 18:27:14 +0200

    add vim plug

--- setup.sh
+++ setup.sh
@@ -8,15 +8,28 @@ fi
 
 if [[ $PWD = */dotfiles ]]; then
 
-	git submodule init
-	git submodule update --init --recursive 
+	# make this file delete itself to not clutter the home directory
+	# setups are one-off 
+	rm setup.sh
+
+	# git submodule init
+	# git submodule update --init --recursive 
 
 	mv * ../
 	mv .* ../
 
-	if ! which apt-get > /dev/null; then 
-		apt-get update 
-		apt-get install vim 
+	cd ..
+
+	if which apt-get > /dev/null; then 
+		yes | apt-get update 
+		yes | apt-get install vim 
 	fi
 
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+	rm -r dotfiles
+
+	# open a new shell
+	exec bash
 fi
